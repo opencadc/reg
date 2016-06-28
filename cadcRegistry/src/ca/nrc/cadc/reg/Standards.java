53251d4fa951fc49ed1ddceda5db6ba0151647c2
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2016-06-27 17:32:00 -0700

    Story 1849. Added standard URI constants to avoid clients having to handle URISyntaxException.

--- cadcRegistry/src/ca/nrc/cadc/reg/Standards.java
+++ cadcRegistry/src/ca/nrc/cadc/reg/Standards.java
@@ -69,6 +69,7 @@
 
 package ca.nrc.cadc.reg;
 
+import java.net.URI;
 
 /**
  * This class defines the constants for the strings that conform to the IVOA 
@@ -132,4 +133,114 @@ public class Standards
     public final static String UMS_MODPASS_10 = "vos://cadc.nrc.ca~vospace/CADC/std/UMS#modpass-1.0";
     public final static String UMS_RESETPASS_10 = "vos://cadc.nrc.ca~vospace/CADC/std/UMS#resetpass-1.0";
     public final static String UMS_WHOAMI_10 = "vos://cadc.nrc.ca~vospace/CADC/std/UMS#whoami-1.0";
+    
+    /* URI corresponding to the above strings */
+    public final static URI SIA_10_URI;
+    public final static URI SIA_QUERY_20_URI;
+
+    public final static URI SODA_SYNC_10_URI;
+    public final static URI SODA_ASYNC_10_URI;
+
+    public final static URI AD_10_URI;
+    public final static URI AD_SYNC_10_URI;
+    public final static URI AD_ASYNC_10_URI;
+    public final static URI AD_TABLES_10_URI;
+
+    public final static URI CAT_10_URI;
+    public final static URI CAT_SYNC_10_URI;
+    public final static URI CAT_ASYNC_10_URI;
+    public final static URI CAT_TABLES_10_URI;
+
+    public final static URI TAP_10_URI;
+    public final static URI TAP_SYNC_11_URI;
+    public final static URI TAP_ASYNC_11_URI;
+    public final static URI TAP_TABLES_11_URI;
+
+    public final static URI VOSPACE_URI;
+    public final static URI VOSPACE_NODES_20_URI;
+    public final static URI VOSPACE_SYNC_20_URI;
+    public final static URI VOSPACE_TRANSFERS_20_URI;
+
+    public final static URI CRED_10_URI;
+    public final static URI CRED_DELEGATE_10_URI;
+    public final static URI CRED_PROXY_10_URI;
+
+    public final static URI DATA_10_URI;
+    public final static URI CANFARDATA_10_URI;
+
+    public final static URI CAOM2REPO_20_URI;
+    public final static URI CUTOUT_20_URI;
+    public final static URI DATALINK_20_URI;
+    public final static URI META_20_URI;
+    public final static URI PKG_20_URI;
+
+    public final static URI PROC_10_URI;
+    public final static URI VMOD_10_URI;
+
+    public final static URI GMS_10_URI;
+    public final static URI GMS_GROUPS_10_URI;
+    public final static URI GMS_SEARCH_10_URI;
+
+    public final static URI UMS_USERS_10_URI;
+    public final static URI UMS_REGS_10_URI;
+    public final static URI UMS_LOGIN_10_URI;
+    public final static URI UMS_MODPASS_10_URI;
+    public final static URI UMS_RESETPASS_10_URI;
+    public final static URI UMS_WHOAMI_10_URI;
+    
+    static 
+    {
+        try 
+        {
+        	SIA_10_URI = URI.create(SIA_10);
+        	SIA_QUERY_20_URI = URI.create(SIA_QUERY_20);
+        	SODA_SYNC_10_URI = URI.create(SODA_SYNC_10);
+        	SODA_ASYNC_10_URI = URI.create(SODA_ASYNC_10);
+        	AD_10_URI = URI.create(AD_10);
+        	AD_SYNC_10_URI = URI.create(AD_SYNC_10);
+        	AD_ASYNC_10_URI = URI.create(AD_ASYNC_10);
+        	AD_TABLES_10_URI = URI.create(AD_TABLES_10);
+        	CAT_10_URI = URI.create(CAT_10);
+        	CAT_SYNC_10_URI = URI.create(CAT_SYNC_10);
+        	CAT_ASYNC_10_URI = URI.create(CAT_ASYNC_10);
+        	CAT_TABLES_10_URI = URI.create(CAT_TABLES_10);
+        	TAP_10_URI = URI.create(TAP_10);
+        	TAP_SYNC_11_URI = URI.create(TAP_SYNC_11);
+        	TAP_ASYNC_11_URI = URI.create(TAP_ASYNC_11);
+        	TAP_TABLES_11_URI = URI.create(TAP_TABLES_11);
+        	VOSPACE_URI = URI.create(VOSPACE);
+            VOSPACE_NODES_20_URI = URI.create(VOSPACE_NODES_20);
+            VOSPACE_SYNC_20_URI = URI.create(VOSPACE_SYNC_20);
+            VOSPACE_TRANSFERS_20_URI = URI.create(VOSPACE_TRANSFERS_20);
+            CRED_10_URI = URI.create(CRED_10);
+            CRED_DELEGATE_10_URI = URI.create(CRED_DELEGATE_10);
+            CRED_PROXY_10_URI = URI.create(CRED_PROXY_10);
+            DATA_10_URI = URI.create(DATA_10);
+            CANFARDATA_10_URI = URI.create(CANFARDATA_10);
+            CAOM2REPO_20_URI = URI.create(CAOM2REPO_20);
+            CUTOUT_20_URI = URI.create(CUTOUT_20);
+            DATALINK_20_URI = URI.create(DATALINK_20);
+            META_20_URI = URI.create(META_20);
+            PKG_20_URI = URI.create(PKG_20);
+            PROC_10_URI = URI.create(PROC_10);
+            VMOD_10_URI = URI.create(VMOD_10);
+            GMS_10_URI = URI.create(GMS_10);
+            GMS_GROUPS_10_URI = URI.create(GMS_GROUPS_10);
+            GMS_SEARCH_10_URI = URI.create(GMS_SEARCH_10);
+            UMS_USERS_10_URI = URI.create(UMS_USERS_10);
+            UMS_REGS_10_URI = URI.create(UMS_REGS_10);
+            UMS_LOGIN_10_URI = URI.create(UMS_LOGIN_10);
+            UMS_MODPASS_10_URI = URI.create(UMS_MODPASS_10);
+            UMS_RESETPASS_10_URI = URI.create(UMS_RESETPASS_10);
+            UMS_WHOAMI_10_URI = URI.create(UMS_WHOAMI_10);
+        } 
+        catch(IllegalArgumentException bug)
+        {
+            throw new RuntimeException("BUG: invalid URI string in static standard constants", bug);
+        }
+        catch(NullPointerException bug)
+        {
+            throw new RuntimeException("BUG: null URI string in static standard constants", bug);
+        }
+    }    
 }
