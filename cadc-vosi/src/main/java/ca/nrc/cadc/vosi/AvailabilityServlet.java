0f7ee31a61079a9d610fed8107d0e07a8b4a15ff
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-02-06 16:56:40 -0800

    Add clientIP to comment of VOSI output.

--- cadc-vosi/src/main/java/ca/nrc/cadc/vosi/AvailabilityServlet.java
+++ cadc-vosi/src/main/java/ca/nrc/cadc/vosi/AvailabilityServlet.java
@@ -3,6 +3,8 @@ package ca.nrc.cadc.vosi;
 import ca.nrc.cadc.auth.AuthenticationUtil;
 import ca.nrc.cadc.log.ServletLogInfo;
 import ca.nrc.cadc.log.WebServiceLogInfo;
+import ca.nrc.cadc.net.NetUtil;
+
 import java.io.IOException;
 import java.security.PrivilegedExceptionAction;
 import javax.security.auth.Subject;
@@ -12,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.jdom2.Document;
 import org.jdom2.output.Format;
@@ -21,38 +24,42 @@ import org.jdom2.output.XMLOutputter;
 /**
  * Servlet implementation class CapabilityServlet
  */
-public class AvailabilityServlet extends HttpServlet
-{
+public class AvailabilityServlet extends HttpServlet {
     private static Logger log = Logger.getLogger(AvailabilityServlet.class);
     private static final long serialVersionUID = 201003131300L;
 
     private String wsClassName;
-    
+
     @Override
     public void init(ServletConfig config)
-        throws ServletException
-    {
+        throws ServletException {
         this.wsClassName = config.getInitParameter("ca.nrc.cadc.vosi.WebService");
         log.info("WebService class name: " + wsClassName);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException
-    {
+        throws ServletException, IOException {
         boolean started = false;
         WebServiceLogInfo logInfo = new ServletLogInfo(request);
         long start = System.currentTimeMillis();
-        try
-        {
+        try {
             Subject subject = AuthenticationUtil.getSubject(request);
             logInfo.setSubject(subject);
             log.info(logInfo.start());
-            
+
             Class wsClass = Class.forName(wsClassName);
             WebService ws = (WebService) wsClass.newInstance();
 
             AvailabilityStatus status = ws.getStatus();
+
+            // TODO: Setting the client IP here may be destructive as it overrides whatever was (potentially) set in the
+            // TODO: WebService instance's constructor.
+            //
+            // TODO: jenkinsd 2018.02.06
+            //
+            status.setClientIP(NetUtil.getClientIP(request));
+
             Availability availability = new Availability(status);
 
             Document document = availability.toXmlDocument();
@@ -60,21 +67,16 @@ public class AvailabilityServlet extends HttpServlet
             started = true;
             response.setContentType("text/xml");
             xop.output(document, response.getOutputStream());
-            
+
             logInfo.setSuccess(true);
-        }
-        catch (Throwable t)
-        {
+        } catch (Throwable t) {
             log.error("BUG", t);
-            if (!started)
-            {
+            if (!started) {
                 response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE, t.getMessage());
             }
             logInfo.setSuccess(false);
             logInfo.setMessage(t.toString());
-        }
-        finally
-        {
+        } finally {
             logInfo.setElapsedTime(System.currentTimeMillis() - start);
             log.info(logInfo.end());
         }
@@ -82,13 +84,10 @@ public class AvailabilityServlet extends HttpServlet
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException
-    {
-        boolean started = false;
+        throws ServletException, IOException {
         WebServiceLogInfo logInfo = new ServletLogInfo(request);
         long start = System.currentTimeMillis();
-        try
-        {
+        try {
             Subject subject = AuthenticationUtil.getSubject(request);
             logInfo.setSubject(subject);
             log.info(logInfo.start());
@@ -99,39 +98,29 @@ public class AvailabilityServlet extends HttpServlet
             Subject.doAs(subject, new ChangeServiceState(ws, request));
 
             response.sendRedirect(request.getRequestURL().toString());
-            
+
             logInfo.setSuccess(true);
-        }
-        catch (Throwable t)
-        {
+        } catch (Throwable t) {
             log.error("BUG", t);
-            if (!started)
-            {
-                response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE, t.getMessage());
-            }
+            response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE, t.getMessage());
             logInfo.setSuccess(false);
             logInfo.setMessage(t.toString());
-        }
-        finally
-        {
+        } finally {
             logInfo.setElapsedTime(System.currentTimeMillis() - start);
             log.info(logInfo.end());
         }
     }
 
-    private class ChangeServiceState implements PrivilegedExceptionAction
-    {
+    private class ChangeServiceState implements PrivilegedExceptionAction {
         private WebService ws;
         private HttpServletRequest req;
 
-        ChangeServiceState(WebService ws, HttpServletRequest req)
-        {
+        ChangeServiceState(WebService ws, HttpServletRequest req) {
             this.ws = ws;
             this.req = req;
         }
 
-        public Object run() throws Exception
-        {
+        public Object run() throws Exception {
             String state = req.getParameter("state");
             ws.setState(state);
             return null;
--- cadc-vosi/src/test/java/ca/nrc/cadc/vosi/AvailabilityTest.java
+++ cadc-vosi/src/test/java/ca/nrc/cadc/vosi/AvailabilityTest.java
@@ -1,71 +1,71 @@
 /*
-************************************************************************
-*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
-**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
-*
-*  (c) 2009.                            (c) 2009.
-*  Government of Canada                 Gouvernement du Canada
-*  National Research Council            Conseil national de recherches
-*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
-*  All rights reserved                  Tous droits réservés
-*
-*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
-*  expressed, implied, or               énoncée, implicite ou légale,
-*  statutory, of any kind with          de quelque nature que ce
-*  respect to the software,             soit, concernant le logiciel,
-*  including without limitation         y compris sans restriction
-*  any warranty of merchantability      toute garantie de valeur
-*  or fitness for a particular          marchande ou de pertinence
-*  purpose. NRC shall not be            pour un usage particulier.
-*  liable in any event for any          Le CNRC ne pourra en aucun cas
-*  damages, whether direct or           être tenu responsable de tout
-*  indirect, special or general,        dommage, direct ou indirect,
-*  consequential or incidental,         particulier ou général,
-*  arising from the use of the          accessoire ou fortuit, résultant
-*  software.  Neither the name          de l'utilisation du logiciel. Ni
-*  of the National Research             le nom du Conseil National de
-*  Council of Canada nor the            Recherches du Canada ni les noms
-*  names of its contributors may        de ses  participants ne peuvent
-*  be used to endorse or promote        être utilisés pour approuver ou
-*  products derived from this           promouvoir les produits dérivés
-*  software without specific prior      de ce logiciel sans autorisation
-*  written permission.                  préalable et particulière
-*                                       par écrit.
-*
-*  This file is part of the             Ce fichier fait partie du projet
-*  OpenCADC project.                    OpenCADC.
-*
-*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
-*  you can redistribute it and/or       vous pouvez le redistribuer ou le
-*  modify it under the terms of         modifier suivant les termes de
-*  the GNU Affero General Public        la “GNU Affero General Public
-*  License as published by the          License” telle que publiée
-*  Free Software Foundation,            par la Free Software Foundation
-*  either version 3 of the              : soit la version 3 de cette
-*  License, or (at your option)         licence, soit (à votre gré)
-*  any later version.                   toute version ultérieure.
-*
-*  OpenCADC is distributed in the       OpenCADC est distribué
-*  hope that it will be useful,         dans l’espoir qu’il vous
-*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
-*  without even the implied             GARANTIE : sans même la garantie
-*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
-*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
-*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
-*  General Public License for           Générale Publique GNU Affero
-*  more details.                        pour plus de détails.
-*
-*  You should have received             Vous devriez avoir reçu une
-*  a copy of the GNU Affero             copie de la Licence Générale
-*  General Public License along         Publique GNU Affero avec
-*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
-*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
-*                                       <http://www.gnu.org/licenses/>.
-*
-*  $Revision: 4 $
-*
-************************************************************************
-*/
+ ************************************************************************
+ *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+ **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+ *
+ *  (c) 2009.                            (c) 2009.
+ *  Government of Canada                 Gouvernement du Canada
+ *  National Research Council            Conseil national de recherches
+ *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+ *  All rights reserved                  Tous droits réservés
+ *
+ *  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+ *  expressed, implied, or               énoncée, implicite ou légale,
+ *  statutory, of any kind with          de quelque nature que ce
+ *  respect to the software,             soit, concernant le logiciel,
+ *  including without limitation         y compris sans restriction
+ *  any warranty of merchantability      toute garantie de valeur
+ *  or fitness for a particular          marchande ou de pertinence
+ *  purpose. NRC shall not be            pour un usage particulier.
+ *  liable in any event for any          Le CNRC ne pourra en aucun cas
+ *  damages, whether direct or           être tenu responsable de tout
+ *  indirect, special or general,        dommage, direct ou indirect,
+ *  consequential or incidental,         particulier ou général,
+ *  arising from the use of the          accessoire ou fortuit, résultant
+ *  software.  Neither the name          de l'utilisation du logiciel. Ni
+ *  of the National Research             le nom du Conseil National de
+ *  Council of Canada nor the            Recherches du Canada ni les noms
+ *  names of its contributors may        de ses  participants ne peuvent
+ *  be used to endorse or promote        être utilisés pour approuver ou
+ *  products derived from this           promouvoir les produits dérivés
+ *  software without specific prior      de ce logiciel sans autorisation
+ *  written permission.                  préalable et particulière
+ *                                       par écrit.
+ *
+ *  This file is part of the             Ce fichier fait partie du projet
+ *  OpenCADC project.                    OpenCADC.
+ *
+ *  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+ *  you can redistribute it and/or       vous pouvez le redistribuer ou le
+ *  modify it under the terms of         modifier suivant les termes de
+ *  the GNU Affero General Public        la “GNU Affero General Public
+ *  License as published by the          License” telle que publiée
+ *  Free Software Foundation,            par la Free Software Foundation
+ *  either version 3 of the              : soit la version 3 de cette
+ *  License, or (at your option)         licence, soit (à votre gré)
+ *  any later version.                   toute version ultérieure.
+ *
+ *  OpenCADC is distributed in the       OpenCADC est distribué
+ *  hope that it will be useful,         dans l’espoir qu’il vous
+ *  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+ *  without even the implied             GARANTIE : sans même la garantie
+ *  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+ *  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+ *  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+ *  General Public License for           Générale Publique GNU Affero
+ *  more details.                        pour plus de détails.
+ *
+ *  You should have received             Vous devriez avoir reçu une
+ *  a copy of the GNU Affero             copie de la Licence Générale
+ *  General Public License along         Publique GNU Affero avec
+ *  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+ *  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+ *                                       <http://www.gnu.org/licenses/>.
+ *
+ *  $Revision: 4 $
+ *
+ ************************************************************************
+ */
 
 package ca.nrc.cadc.vosi;
 
@@ -80,7 +80,7 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -94,43 +94,38 @@ import ca.nrc.cadc.date.DateUtil;
 import ca.nrc.cadc.util.Log4jInit;
 import ca.nrc.cadc.xml.XmlUtil;
 
+
 /**
  * @author zhangsa
- *
  */
-public class AvailabilityTest
-{
+public class AvailabilityTest {
     private static Logger log = Logger.getLogger(AvailabilityTest.class);
-    static
-    {
+
+    static {
         Log4jInit.setLevel("ca.nrc.cadc.vosi", Level.INFO);
     }
 
-    Map<String,String> schemaMap = new HashMap<String,String>();
-    DateFormat df = DateUtil.getDateFormat(DateUtil.IVOA_DATE_FORMAT, DateUtil.LOCAL);
+    private final Map<String, String> schemaMap = new HashMap<>();
+    private final DateFormat df = DateUtil.getDateFormat(DateUtil.IVOA_DATE_FORMAT, DateUtil.LOCAL);
 
     /**
-     * @throws java.lang.Exception
+     * @throws java.lang.Exception For any errors during setup.
      */
     @Before
-    public void setUp() throws Exception
-    {
-        this.schemaMap.put( VOSI.AVAILABILITY_NS_URI, XmlUtil.getResourceUrlString(VOSI.AVAILABILITY_SCHEMA, AvailabilityTest.class));
+    public void setUp() throws Exception {
+        this.schemaMap.put(VOSI.AVAILABILITY_NS_URI, XmlUtil.getResourceUrlString(VOSI.AVAILABILITY_SCHEMA,
+                                                                                  AvailabilityTest.class));
     }
 
     @Test
-    public void testAvailability() throws Exception
-    {
+    public void testAvailability() throws Exception {
         AvailabilityStatus status = null;
-        try
-        {
+        try {
             Date d1 = df.parse("2009-04-12T11:22:33.444"); //yyyy-MM-dd'T'HH:mm:ss.SSS
             Date d2 = df.parse("2009-05-12T11:22:33.444"); //yyyy-MM-dd'T'HH:mm:ss.SSS
             Date d3 = df.parse("2009-06-12T11:22:33.444"); //yyyy-MM-dd'T'HH:mm:ss.SSS
-            status = new AvailabilityStatus(true, d1, d2, d3, "noteA");
-        }
-        catch (ParseException e)
-        {
+            status = new AvailabilityStatus(true, d1, d2, d3, "noteA", "192.168.1.3");
+        } catch (ParseException e) {
             log.error("test code bug", e);
         }
         Availability availability = new Availability(status);
@@ -148,14 +143,13 @@ public class AvailabilityTest
         TestUtil.assertXmlNode(doc, "/vosi:availability/vosi:downAt", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
         TestUtil.assertXmlNode(doc, "/vosi:availability/vosi:backAt", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
         TestUtil.assertXmlNode(doc, "/vosi:availability/vosi:note", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
-
+        Assert.assertTrue("IP Address comment missing.",
+                          TestUtil.hasCommentContaining(doc.getRootElement(), "Client IP: 192.168.1.3"));
     }
 
     @Test
-    public void testAvailabilityEmptyStatus() throws Exception
-    {
-        AvailabilityStatus status = null;
-        status = new AvailabilityStatus(false, null, null, null, null);
+    public void testAvailabilityEmptyStatus() throws Exception {
+        AvailabilityStatus status = new AvailabilityStatus(false, null, null, null, null, null);
         Availability availability = new Availability(status);
         Document doc = availability.toXmlDocument();
         XMLOutputter xop = new XMLOutputter(Format.getPrettyFormat());
@@ -166,7 +160,8 @@ public class AvailabilityTest
         XmlUtil.buildDocument(reader, schemaMap);
 
         TestUtil.assertXmlNode(doc, "/vosi:availability", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
-        TestUtil.assertXmlNode(doc, "/vosi:availability/vosi:available[.='false']", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
+        TestUtil.assertXmlNode(doc, "/vosi:availability/vosi:available[.='false']", VOSI.NS_PREFIX, VOSI
+            .AVAILABILITY_NS_URI);
         TestUtil.assertNoXmlNode(doc, "/vosi:availability/vosi:upSince", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
         TestUtil.assertNoXmlNode(doc, "/vosi:availability/vosi:downAt", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
         TestUtil.assertNoXmlNode(doc, "/vosi:availability/vosi:backAt", VOSI.NS_PREFIX, VOSI.AVAILABILITY_NS_URI);
@@ -174,14 +169,14 @@ public class AvailabilityTest
     }
 
     @Test
-    public void testAvailabilityRoundTrip() throws Exception
-    {
+    public void testAvailabilityRoundTrip() throws Exception {
         Calendar c1 = new GregorianCalendar();
         Calendar c2 = new GregorianCalendar();
         Calendar c3 = new GregorianCalendar();
         c2.add(Calendar.MONTH, 1);
         c3.add(Calendar.MONTH, 2);
-        AvailabilityStatus status1 = new AvailabilityStatus(false, c1.getTime(), c2.getTime(), c3.getTime(), "status message");
+        AvailabilityStatus status1 = new AvailabilityStatus(false, c1.getTime(), c2.getTime(), c3.getTime(), "status " +
+            "message");
 
         Availability availability = new Availability(status1);
         Document doc = availability.toXmlDocument();
